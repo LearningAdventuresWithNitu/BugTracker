f731bedb6327493d31ff2b1b2c641dfa7a4b97a1
Nitu Choudhary <learningadventureswithnitu@gmail.com>
2024-04-04 23:39:11 -0400

    first commit

---
+++ js/config.js
@@ -0,0 +1,109 @@
+// Use Remix to deploy smart contract to local Ganache blockchain.
+// Make note of the address the contract was deployed to, and paste it below.
+
+// let contractAddress = '0x9F1209AaAAcbcda461e02A5dAB905c748CDA382f';
+let contractAddress = '0xB50c8E7aD0ad588000603b9C604bf75dDe739Ce3';
+
+// Define the smart contract ABI (Application Binary Interface).
+// This is a JSON representation of the functions in the smart contract.
+let contractABI =
+[
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_description",
+				"type": "string"
+			}
+		],
+		"name": "addBug",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_bugIndex",
+				"type": "uint256"
+			}
+		],
+		"name": "deleteBug",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_bugIndex",
+				"type": "uint256"
+			},
+			{
+				"internalType": "bool",
+				"name": "_status",
+				"type": "bool"
+			}
+		],
+		"name": "updateBugStatus",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getBugCount",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_bugIndex",
+				"type": "uint256"
+			}
+		],
+		"name": "getTask",
+		"outputs": [
+			{
+				"components": [
+					{
+						"internalType": "string",
+						"name": "bugID",
+						"type": "string"
+					},
+					{
+						"internalType": "string",
+						"name": "description",
+						"type": "string"
+					},
+					{
+						"internalType": "enum BugTracker.criticalLevels",
+						"name": "level",
+						"type": "uint8"
+					},
+					{
+						"internalType": "bool",
+						"name": "isDone",
+						"type": "bool"
+					}
+				],
+				"internalType": "struct BugTracker.Bug",
+				"name": "",
+				"type": "tuple"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
